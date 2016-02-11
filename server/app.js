76968571cde521cdbfdd6e1da3b9257a6259ae52
Metabaron <Metabaron@MacBook-Pro-de-Desarrollo.local>
2016-02-11 15:45:48 -0600

    dist version

---
+++ server/app.js
@@ -0,0 +1,33 @@
+/**
+ * Main application file
+ */
+
+'use strict';
+
+// Set default node environment to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var express = require('express');
+var config = require('./config/environment');
+// Setup server
+var app = express();
+var server = require('http').createServer(app);
+var fs = require('fs');
+require('./config/express')(app);
+require('./routes')(app);
+
+var io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+  console.log('___------______------_____ NEW EVENT!');
+});
+
+app.set('io', io);
+
+// Start server
+server.listen(config.port, config.ip, function () {
+  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+});
+
+// Expose app
+exports = module.exports = app;

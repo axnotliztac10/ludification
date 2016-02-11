bbda40904f76a9f68a20a5c92ccb5f759f3d29a3
Metabaron <Metabaron@MacBook-Pro-de-Desarrollo.local>
2016-02-11 15:45:48 -0600

    dist version

---
+++ dist/server/api/thing/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var express = require('express');
+var controller = require('./thing.controller');
+
+var router = express.Router();
+router.post('/', controller.index);
+
+module.exports = router;
